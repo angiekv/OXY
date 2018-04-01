8b060bec2e863c52ae23734c9ceab6352e742611
BaDiakite <BaDiakite@RIFP00571.cridf.iledefrance.fr>
2018-04-01 17:00:21 +0200

    Junit customer fonctionnel

--- test/Model/DAOCustomerTest.java
+++ test/Model/DAOCustomerTest.java
@@ -1,9 +1,11 @@
+package Model;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+
 
 import java.util.List;
 import org.junit.After;
@@ -14,6 +16,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import Server.DAOCustomer;
 import java.sql.Connection;
+import java.sql.SQLException;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 /**
@@ -21,7 +24,12 @@ import org.junit.runner.RunWith;
  * @author OXY
  */
 public class DAOCustomerTest {
+    private static Connection con;
     
+    public static Connection Con () throws SQLException{
+        con = Database.getConnection();
+        return con;
+    }
     public DAOCustomerTest() {
     }
     
@@ -46,48 +54,49 @@ public class DAOCustomerTest {
      * @param c
      * @throws java.lang.Exception
      */
-    public void testloadCustomer(Connection c) throws Exception {
-        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(c);
+    @Test
+    public void testloadCustomer() throws Exception {
+        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(Con());
         assertNotNull(lesClients);
     }
 
-//    /**
-//     * Test of updateCustomer method, of class DAOCustomer.
-//     */
-//    @Test
-//    public void testupdateCustomer(Connection c) throws Exception {
-//        System.out.println("updateCustomer");
-//        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(c);
-//        Server.Customer m = lesClients.get(2);
-//        DAOCustomer.updateCustomer(c,1, "Costa", "Rui","15 rue Paul","77290","Paris","ruicosta@hotmail.fr","M");
-//        List<Server.Customer> results = DAOCustomer.loadCustomer(c);
-//        Server.Customer r = results.get(4);
-//        assertFalse(m.getPrenom() == r.getPrenom());
-//        
-//    }
-//
-//    /**
-//     * Test of deleteCustomer method, of class DAOCustomer.
-//     */
-//    @Test
-//    public void testdeleteCustomer(Connection c) throws Exception {
-//        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(c);
-//        DAOCustomer.deleteCustomer(c,1);
-//        List<Server.Customer> r = DAOCustomer.loadCustomer(c);
-//        assertEquals(lesClients.size()-1 , r.size());
-//        
-//        
-//    }
-//
-//    /**
-//     * Test of addCustomer method, of class DAOCustomer.
-//     */
-//    @Test
-//    public void testaddCustomer(Connection c) throws Exception {
-//        System.out.println("addCustomer");
-//        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(c);
-//        DAOCustomer.addCustomer(c,"Costa", "Mario","15 rue Paul","77290","Paris","ruicosta@hotmail.fr","M");
-//        List<Server.Customer> r = DAOCustomer.loadCustomer(c);
-//        assertEquals(lesClients.size()+1 , r.size());
-//    }
+    /**
+     * Test of updateCustomer method, of class DAOCustomer.
+     */
+    @Test
+    public void testupdateCustomer() throws Exception {
+        System.out.println("updateCustomer");
+        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(Con());
+        Server.Customer m = lesClients.get(2);
+        DAOCustomer.updateCustomer(Con(),1, "Costa", "Rui","15 rue Paul","77290","Paris","ruicosta@hotmail.fr","M");
+        List<Server.Customer> results = DAOCustomer.loadCustomer(Con());
+        Server.Customer r = results.get(4);
+        assertFalse(m.getPrenom() == r.getPrenom());
+        
+    }
+
+    /**
+     * Test of deleteCustomer method, of class DAOCustomer.
+     */
+    @Test
+    public void testdeleteCustomer() throws Exception {
+        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(Con());
+        DAOCustomer.deleteCustomer(Con(),1);
+        List<Server.Customer> r = DAOCustomer.loadCustomer(Con());
+        assertEquals(lesClients.size()-1 , r.size());
+        
+        
+    }
+
+    /**
+     * Test of addCustomer method, of class DAOCustomer.
+     */
+    @Test
+    public void testaddCustomer() throws Exception {
+        System.out.println("addCustomer");
+        List<Server.Customer> lesClients = DAOCustomer.loadCustomer(Con());
+        DAOCustomer.addCustomer(Con(),"Costa", "Mario","15 rue Paul","77290","Paris","ruicosta@hotmail.fr","M");
+        List<Server.Customer> r = DAOCustomer.loadCustomer(Con());
+        assertEquals(lesClients.size()+1 , r.size());
+    }
 }
