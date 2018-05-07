566952414a58ff03c4a53291676bbc4f0292c883
Aberkane <Aberkane@DESKTOP-HD9F98R>
2018-05-07 15:35:21 +0200

    Use case MAPPING MAGASIN EMPLACEMENT / AFFECTLOCATION TEST / ET modification DES CLASSE

--- src/Server/DAOShop.java
+++ src/Server/DAOShop.java
@@ -26,7 +26,7 @@ public class DAOShop {
  */
     
     public synchronized static List<Shop> loadShops(Connection c) throws SQLException {
-        //list of customer
+        //list of shop
         List<Shop> listShop = new ArrayList<>();
         Statement myStmt = c.createStatement();
         //The query which selects all the shops.
@@ -36,19 +36,29 @@ public class DAOShop {
             int id = myRs.getInt("idMagasin");
             String designation = myRs.getString("designation");
             String description = myRs.getString("description");
-            int rent = myRs.getInt("loyer");
+            int loyer = myRs.getInt("loyer");
             int surface = myRs.getInt("superficie");
-            int floor = myRs.getInt("niveau");
-            String localization = myRs.getString("localisation");
-            Shop M = new Shop(id, designation, description, rent,surface,floor,localization);
+            int niveau = myRs.getInt("niveau");
+            String localisation = myRs.getString("localisation");
+            //liste type 
+            Statement myStmt2 = c.createStatement();
+            //The query which selects all the shops.
+            ResultSet myRs2 = myStmt2.executeQuery("SELECT designation,idType from magasin_has_type,type where magasin_has_type.type_idType=type.idType and magasin_has_type.magasin_idMagasin=" + id);
+            List<Type> list = new ArrayList<>();
+            while (myRs2.next()) {
+                int idtype = myRs2.getInt("idType");
+                String designationType = myRs2.getString("designation");
+                Type T = new Type(idtype, designationType);
+                list.add(T);
+            }
+            Shop M = new Shop(id, designation, description, loyer, surface, niveau, localisation, list);
 
             listShop.add(M);
         }
-        myStmt.close(); 
+        myStmt.close();
         return listShop;
-        
 
-    }         
+    }        
     /**
      * This method returns only the shops which types matches a customer's profile
      * @param c
@@ -92,69 +102,92 @@ public class DAOShop {
  * @param c
  * @throws SQLException 
  */
-    public synchronized void updateShop(int idShop, String designation, String description,int rent,int surface ,int floor ,String localization, int idType ,Connection c) throws SQLException {
-        
-        PreparedStatement myStmt=null;
+   public synchronized void updateShop(int idShop, String designation, String description, int loyer, int surface, int niveau, String localisation, List<Integer> ListidTypeOld, List<Integer> ListidTypeNew, Connection c) throws SQLException {
+        //The query which selects all the shops.
+
+        PreparedStatement myStmt2 = null;
         //The query which modifies one or more attributes of a shop.
-        myStmt = c.prepareStatement("update magasin set designation=? ,description=?, loyer=? , superficie=? , niveau=? , localisation=?, where idMagasin=? ");
-        
-        
+        myStmt2 = c.prepareStatement("update magasin set designation=? ,description=?, loyer=? , superficie=? , niveau=? , localisation=? where idMagasin=? ");
+
         //The attributes' values are placed in the same order as "?".
-        myStmt.setString(1, designation);
-        myStmt.setString(2, description);
-        myStmt.setInt(3, rent);
-        myStmt.setInt(4, surface);
-        myStmt.setInt(5, floor);
-        myStmt.setString(6, localization);
-        myStmt.setInt(7, idShop);
-        myStmt.executeUpdate();
+        myStmt2.setString(1, designation);
+        myStmt2.setString(2, description);
+        myStmt2.setInt(3, loyer);
+        myStmt2.setInt(4, surface);
+        myStmt2.setInt(5, niveau);
+        myStmt2.setString(6, localisation);
+        myStmt2.setInt(7, idShop);
+        myStmt2.executeUpdate();
         //This request update the type of the shop ;
-        PreparedStatement myStmt2=null;
-        myStmt2= c.prepareStatement("update magasin_has_type set type_idType=? where magasin_idMagasin=?");
-        myStmt2.setInt(1, idType);
-        myStmt2.setInt(2, idShop);
-        myStmt.executeUpdate();
+        PreparedStatement myStmt3 = null;
+        int sizeOld = ListidTypeOld.size();
+        int indexTabNew=0;
+        for (int i = 0; i < sizeOld; i++) {//  loop for type of old with index i
+            int idType = ListidTypeOld.get(i);
+            if (ListidTypeNew.contains(indexTabNew)) {
+                for(int j = 0; j < ListidTypeNew.size(); j++){// boulce for delete the type if this type is in both list (old and anew ) index j 
+                    if(ListidTypeNew.get(j)==idType){
+                        ListidTypeNew.remove(j);
+                        
+                        break;
+                    }
+                }
+                ListidTypeOld.remove(i);
+                indexTabNew++;
+            }
+
+        }
+        if (!ListidTypeNew.isEmpty()) {
+            for (int newIdType : ListidTypeNew) {
+
+                int idTypeOld = ListidTypeOld.get(0);
+                myStmt3 = c.prepareStatement("update magasin_has_type set type_idType=? where magasin_idMagasin=? and type_idType= ?");
+                myStmt3.setInt(1, newIdType);
+                myStmt3.setInt(2, idShop);
+                myStmt3.setInt(3, idTypeOld);
+                myStmt3.executeUpdate();
+                ListidTypeOld.remove(0);
+
+            }
+        }
+
     }
-/**
- * This method deletes a shop from the database.
- * @param idShop
+
+    /**
+     * This method deletes a shop from the database.
+     *
+     * @param idShop
      * @param c
- * @throws SQLException 
- */
-    public synchronized void deleteShop(int idShop,Connection c) throws SQLException {
-        
-        PreparedStatement myStmt=null;
+     * @throws SQLException
+     */
+    public synchronized void deleteShop(int idShop, Connection c) throws SQLException {
+
+        PreparedStatement myStmt = null;
         //The query deletes type of the shop we are going to delete from the database.
         myStmt = c.prepareStatement("delete From magasin_has_type where magasin_idMagasin=?");
         myStmt.setInt(1, idShop);
         myStmt.executeUpdate();
-        
-        PreparedStatement myStmt2=null;
-        
+
+        PreparedStatement myStmt2 = null;
+
         //The query deletes a shop from the database.
         myStmt2 = c.prepareStatement("delete From magasin where idMagasin=?");
         myStmt2.setInt(1, idShop);
         myStmt2.executeUpdate();;
     }
-/**
- * This method adds a shop to the database
- * @param designation
- * @param description
- * @param idType
- * @throws SQLException 
- */
-    public synchronized void addShop(String designation, String description,int rent,int surface ,int floor ,String localization, int idType ,Connection c) throws SQLException {
+    
+    public synchronized void addShop(String designation, String description, int loyer, int surface, int niveau, String localisation, List<Integer> ListidType, Connection c) throws SQLException {
         //The query adds a shop to the database.
-         PreparedStatement myStmt=null;
+        PreparedStatement myStmt = null;
         myStmt = c.prepareStatement("insert into magasin (designation,description,loyer,superficie,niveau,localisation) values (?,?,?,?,?,?) ");
-        
+
         //The attributes' values are placed in the same order as "?".
         myStmt.setString(1, designation);
         myStmt.setString(2, description);
-        myStmt.setInt(3, rent);
+        myStmt.setInt(3, loyer);
         myStmt.setInt(4, surface);
-        myStmt.setInt(5, floor);
-        myStmt.setString(6, localization);
+        myStmt.setInt(5, niveau);
+        myStmt.setString(6, localisation);
         myStmt.executeUpdate();
         Statement myStmt2 = c.createStatement();
         //The query which selects all the shops.
@@ -162,11 +195,14 @@ public class DAOShop {
         myRs.next();
         int lastid = myRs.getInt("last_id");
         //This request insert the type of the shop in database;
-        PreparedStatement myStmt3=null;
-        myStmt3= c.prepareStatement("insert into magasin_has_type values (?,?)");
-        myStmt3.setInt(1, lastid);
-        myStmt3.setInt(2, idType);
-        myStmt3.executeUpdate();
+        for(int idType :ListidType ){
+            PreparedStatement myStmt3 = null;
+            myStmt3 = c.prepareStatement("insert into magasin_has_type values (?,?)");
+            myStmt3.setInt(1, lastid);
+            myStmt3.setInt(2, idType);
+            myStmt3.executeUpdate();
+        }
+        
 
     }
     
